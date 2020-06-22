968605789801e4bfe6da5ecf3518d9fee72c4cb8
Casey Roby <elenacaseyroby@gmail.com>
2020-06-22 13:24:22 -0400

    async storage works

--- services/asyncStorage.js
+++ services/asyncStorage.js
@@ -3,21 +3,24 @@ import {AsyncStorage} from 'react-native';
 export default {
   async _storeData(key, value) {
     try {
-      await AsyncStorage.setItem(key, value);
-    } catch (error) {
-      console.log(error);
-      return;
+      await AsyncStorage.setItem(key, JSON.stringify(value));
+      console.log('Data successfully saved');
+      return 'success';
+    } catch (e) {
+      console.log('Failed to save the data to the storage');
     }
   },
   async _retrieveData(key) {
     try {
       const value = await AsyncStorage.getItem(key);
+
       if (value !== null) {
-        return value;
+        const parsedValue = await JSON.parse(value);
+        console.log(`parsed vlaue: ${parsedValue}`);
+        return parsedValue;
       }
-    } catch (error) {
-      console.log(error);
-      return;
+    } catch (e) {
+      console.log('Failed to fetch the data from storage');
     }
   },
 };
