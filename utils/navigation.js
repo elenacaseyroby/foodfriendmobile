c4a0983b8817b454d0bb87a062b4004b23cc07d1
Casey Roby <elenacaseyroby@gmail.com>
2020-06-28 20:56:37 -0400

    do nothing if routes to UpdatePassword for logged in user

--- utils/navigation.js
+++ utils/navigation.js
@@ -1,4 +1,6 @@
-export function routeDeepLink(url, {navigate}) {
+import asyncStorage from '../asyncStorage';
+
+export async function routeDeepLink(url, {navigate}) {
   // navigate function is extracted from this.props.navigation
   // and passed into this function.
 
@@ -6,14 +8,25 @@ export function routeDeepLink(url, {navigate}) {
   const route = url.replace(/.*?:\/\//g, '');
   const routeName = route.split('/')[0];
 
-  // Routing to UpdatePassword component:
-  // foodfriend://updatepassword/:id/:token
-  if (routeName === 'updatepassword') {
-    const userId = route.split('/')[1];
-    const passwordResetToken = route.split('/')[2];
-    navigate('UpdatePassword', {
-      userId: userId,
-      passwordResetToken: passwordResetToken,
-    });
+  // Check if user is logged in & allow different routes based on that.
+  const userId = await asyncStorage._getData('USER_ID');
+  const accessToken = await asyncStorage._getData('ACCESS_TOKEN');
+  const loggedIn = userId && accessToken;
+
+  // logged in routing:
+  if (loggedIn) {
+    return;
+    // loged out routing:
+  } else {
+    // Routing to UpdatePassword component:
+    // foodfriend://updatepassword/:id/:token
+    if (routeName === 'updatepassword') {
+      const userId = route.split('/')[1];
+      const passwordResetToken = route.split('/')[2];
+      navigate('UpdatePassword', {
+        userId: userId,
+        passwordResetToken: passwordResetToken,
+      });
+    }
   }
 }
