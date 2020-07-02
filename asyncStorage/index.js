fe455174b8f9b9d88b84afb14a59f706a6f4803e
Casey Roby <elenacaseyroby@gmail.com>
2020-07-02 16:42:47 -0400

    added new package to continue use of AsyncStorage
    Warning: AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-community/async-storage' instead of 'react-native'

--- asyncStorage/index.js
+++ asyncStorage/index.js
@@ -1,15 +1,18 @@
-import {AsyncStorage} from 'react-native';
+import {AsyncStorage} from '@react-native-community/async-storage';
 
 export default {
   async _storeData(key, value) {
+    // output 'success' or null
     try {
       await AsyncStorage.setItem(key, value);
       return 'success';
     } catch (error) {
       console.log('Failed to save the data to the storage');
+      return;
     }
   },
   async _retrieveData(key) {
+    // output 'success' or null
     try {
       const value = await AsyncStorage.getItem(key);
       if (value !== null) {
@@ -17,15 +20,18 @@ export default {
       }
     } catch (error) {
       console.log('Failed to fetch the data from storage');
+      return;
     }
   },
   async _clearData() {
+    // output 'success' or null
     try {
       const keys = await AsyncStorage.getAllKeys();
       await AsyncStorage.multiRemove(keys);
       return 'success';
     } catch (error) {
       console.error('Error clearing app data.');
+      return;
     }
   },
 };
