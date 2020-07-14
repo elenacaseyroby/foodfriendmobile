e6715e8b4f65d976711995a397a4943094015b27
Casey Roby <elenacaseyroby@gmail.com>
2020-07-14 14:33:19 -0400

    start building slide 1

--- components/OnboardingSlides/SlideOne.js
+++ components/OnboardingSlides/SlideOne.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import {View, StyleSheet, ImageBackground} from 'react-native';
+import {View, Text, Image, StyleSheet, ImageBackground} from 'react-native';
 import ProgressBar from './ProgressBar';
 import backgroundImage from './assets/backgroundImage1.png';
+import mirror from './assets/mirror-illustration.png';
+import coins from './assets/money-icon.png';
+import RightArrow from './assets/swipe-right-arrow.svg';
 
 class SlideOne extends React.Component {
   render() {
@@ -9,6 +12,28 @@ class SlideOne extends React.Component {
       <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
         <View style={styles.progressBar}>
           <ProgressBar activeCircleIndex={1} />
+        </View>
+        <View style={styles.everyoneTextContainer}>
+          <Text style={styles.everyoneText}>Everyone</Text>
+          <Text style={styles.deservesText}>deserves to feel good</Text>
+        </View>
+        <Image source={mirror} />
+        <View style={styles.textAndCoinsContainer}>
+          <View style={styles.textContainer}>
+            <Text style={styles.butText}>but</Text>
+            <View style={styles.rowContainer}>
+              <Text style={styles.thiscanBeText}>this can be</Text>
+              <Text style={styles.difficultText}>difficult</Text>
+            </View>
+            <Text style={styles.whenText}>when relying on</Text>
+            <Text style={styles.expensiveText}>expensive, unregulated</Text>
+            <Text style={styles.supplementsText}>supplements.</Text>
+          </View>
+          <Image source={coins} />
+        </View>
+        <View style={styles.rowContainer}>
+          <Text style={styles.swipeText}>Swipe to continue</Text>
+          <RightArrow />
         </View>
       </ImageBackground>
     );
@@ -23,6 +48,30 @@ const styles = StyleSheet.create({
   backgroundImage: {
     height: '100%',
     flex: 1,
+  },
+  everyoneTextContainer: {
+    alignSelf: 'center',
+    alignItems: 'center',
+  },
+  everyoneText: {
+    fontFamily: 'Bellota-Regular',
+  },
+  deservesText: {},
+  textAndCoinsContainer: {
+    flexDirection: 'row',
+  },
+  textContainer: {
+    width: '70%',
+  },
+  butText: {},
+  thiscanBeText: {},
+  difficultText: {},
+  whenText: {},
+  expensiveText: {},
+  supplementsText: {},
+  swipeText: {},
+  rowContainer: {
+    flexDirection: 'row',
   },
 });
 
