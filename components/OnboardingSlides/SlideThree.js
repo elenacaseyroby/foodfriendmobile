63cc398d2af5fb66ff8eb6a0f21f7b13516a4b61
Casey Roby <elenacaseyroby@gmail.com>
2020-07-15 15:09:16 -0400

    add images for remaining slides

--- components/OnboardingSlides/SlideThree.js
+++ components/OnboardingSlides/SlideThree.js
@@ -10,19 +10,42 @@ class SlideThree extends React.Component {
         <View style={styles.progressBar}>
           <ProgressBar activeCircleIndex={3} />
         </View>
+        <View style={styles.rectangle}>
+          <View style={styles.slideContent}></View>
+        </View>
       </ImageBackground>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  // Component styles from top of slide
+  // to bottom of slide.
   progressBar: {
     marginTop: '12%',
     alignSelf: 'center',
+    position: 'absolute',
+  },
+  // Resusable & component wide styles
+  rowContainer: {
+    flexDirection: 'row',
   },
   backgroundImage: {
     height: '100%',
     flex: 1,
+  },
+  // Use this to fit slide on iPhone 8
+  // and center slide on iPhone 11
+  rectangle: {
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  slideContent: {
+    height: 650,
+    width: 375,
+    borderColor: '#ffffff',
+    borderWidth: 0.5,
   },
 });
 
