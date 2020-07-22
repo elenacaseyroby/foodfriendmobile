e2259820a7e3b31ee45b136a84363b4a33c0abd7
Casey Roby <elenacaseyroby@gmail.com>
2020-07-22 14:34:22 -0400

    normalize sign up button

--- components/common/SignUpButton.js
+++ components/common/SignUpButton.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {TouchableOpacity, Text, StyleSheet} from 'react-native';
+import {normalize} from '../../utils/sizeScaling';
 import propTypes from 'prop-types';
 
 class SignUpButton extends React.Component {
@@ -18,15 +19,15 @@ class SignUpButton extends React.Component {
 const styles = StyleSheet.create({
   text: {
     fontFamily: 'Cabin-Regular',
-    fontSize: 17,
+    fontSize: normalize(17),
     color: '#ffffff',
     textAlign: 'center',
   },
   button: {
-    width: 309,
-    height: 57,
-    paddingTop: 17,
-    borderRadius: 28.5,
+    width: normalize(304),
+    height: normalize(57),
+    justifyContent: 'center',
+    borderRadius: normalize(28.5),
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
