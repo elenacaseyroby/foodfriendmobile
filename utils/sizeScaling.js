316f00b5ed39d3835b387a77e0bbd0563edacb4e
Casey Roby <elenacaseyroby@gmail.com>
2020-07-22 14:27:31 -0400

    remove basedOnHeight parameter

--- utils/sizeScaling.js
+++ utils/sizeScaling.js
@@ -4,20 +4,15 @@ import {Dimensions} from 'react-native';
 // 1. can't use async functions in the stylesheet
 // 2. should use get('window') on every render in case phone is tilted
 // to side.
-export function normalize(value, maxValue = null, basedOnHeight = false) {
+export function normalize(value, maxValue = null) {
   // input value based on iPhone 8.
   // output value to scale on current device.
-  // normalizes based on width by default.
-  let multiplier;
-  if (basedOnHeight) {
-    const normalizedHeight = 667;
-    const currentHeight = Dimensions.get('window').height;
-    multiplier = currentHeight / normalizedHeight;
-  } else {
-    const currentWidth = Dimensions.get('window').width;
-    const normalizedWidth = 375;
-    multiplier = currentWidth / normalizedWidth;
-  }
+  // normalizes based on width, because that's consistent even if the
+  // device is flipped.
+
+  const currentWidth = Dimensions.get('window').width;
+  const normalizedWidth = 375;
+  const multiplier = currentWidth / normalizedWidth;
   const normalizedValue = value * multiplier;
   if (maxValue) {
     if (normalizedValue > maxValue) return maxValue;
