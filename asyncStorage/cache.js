cd5a285a3860bc04568d03cd3ee9ed090ab4e927
Casey Roby <elenacaseyroby@gmail.com>
2020-08-06 16:31:45 -0400

    add placeholder for cache

---
+++ asyncStorage/cache.js
@@ -0,0 +1,13 @@
+import asyncStorage from './index';
+
+export function buildOrRetrieveNutrientCache() {}
+
+export function buildOrRetrieveUserCache() {}
+
+export function buildOrRetrievePathCache() {}
+
+export function buildOrRetrieveDietCache() {}
+
+export function buildOrRetrieveTermsCache() {}
+
+export function buildOrRetrievePrivacyPolicyCache() {}
