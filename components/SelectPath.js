6215bd689d790fc22894d77cc46f700b5e3ea1a9
Casey Roby <elenacaseyroby@gmail.com>
2020-08-08 14:33:37 -0400

    Select path back arrow and header image

--- components/SelectPath.js
+++ components/SelectPath.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {ScrollView, View, StyleSheet} from 'react-native';
+import {normalize} from '../utils/deviceScaling';
 import FFStatusBar from './common/FFStatusBar';
+import PathHeader from './common/PathHeader';
+import BackArrow from './common/BackArrow';
 import {connect} from 'react-redux';
 import propTypes from 'prop-types';
 
@@ -19,13 +22,31 @@ class SelectPath extends React.Component {
       <>
         <FFStatusBar />
         {/*scrollIndicatorInsets setting prevents bug: https://github.com/facebook/react-native/issues/26610*/}
-        <ScrollView scrollIndicatorInsets={{right: 1}}></ScrollView>
+        <ScrollView scrollIndicatorInsets={{right: 1}}>
+          <PathHeader />
+          <View style={styles.arrowContainer}>
+            <BackArrow
+              style={styles.backArrow}
+              onPress={() => navigation.pop()}
+            />
+          </View>
+        </ScrollView>
       </>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  arrowContainer: {
+    position: 'absolute',
+    width: normalize(325),
+    alignSelf: 'center',
+    alignItems: 'flex-start',
+  },
+  backArrow: {
+    marginTop: normalize(41),
+  },
+});
 
 const mapStateToProps = (state) => ({
   paths: state.paths,
