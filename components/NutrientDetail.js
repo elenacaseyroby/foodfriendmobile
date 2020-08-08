00d88c8796a083e9a9b9452e32c8b5d01c5203c2
Casey Roby <elenacaseyroby@gmail.com>
2020-08-07 23:09:02 -0400

    almost done

--- components/NutrientDetail.js
+++ components/NutrientDetail.js
@@ -3,10 +3,11 @@ import {ScrollView, Image, Text, View, StyleSheet} from 'react-native';
 import FFStatusBar from './common/FFStatusBar';
 import OfflineNoticeBanner from './common/OfflineNoticeBanner';
 // import topElipse from '../assets/images/';
-import bottomElipse from '../assets/images/bottom-elipse-blue.png';
-import topElipse from '../assets/images/top-elipse-two-toned-orange.png';
+import BlueBottomElipse from './common/BlueBottomElipse';
 import {normalize} from '../utils/deviceScaling';
 import BackArrow from './common/BackArrow';
+import topElipse from '../assets/images/top-elipse-two-toned-orange.png';
+import plantMascot from '../assets/images/plant-mascot-blue.png';
 import propTypes from 'prop-types';
 
 class NutrientDetail extends React.Component {
@@ -78,6 +79,17 @@ class NutrientDetail extends React.Component {
             <Text style={styles.bannerText}>Sources</Text>
           </View>
           <Text style={styles.description}>{nutrient.sourceNote}</Text>
+          <Text style={styles.disclaimer}>
+            Please note, Food Friend does not represent expert advice and should
+            never be used as a substitute for direct medical advice. If you are
+            experiencing a health issue, we ask that you reach out to a
+            healthcare professional. Additionally, we encourage you to explore
+            how the introduction of new nutrients into your diet might impact
+            your health with your doctor or other qualified healthcare provider.
+          </Text>
+          <BlueBottomElipse style={styles.bottomElipse} />
+
+          <Image style={styles.plantMascot} source={plantMascot} />
           <View style={styles.arrowContainer}>
             <BackArrow
               style={styles.backArrow}
@@ -139,6 +151,25 @@ const styles = StyleSheet.create({
     fontFamily: 'Cabin-Regular',
     fontSize: normalize(14),
     color: '#555555',
+  },
+  disclaimer: {
+    alignSelf: 'center',
+    width: normalize(301),
+    color: '#aaaaaa',
+    fontFamily: 'Cabin-Regular',
+    fontSize: normalize(12),
+  },
+  bottomElipse: {
+    marginTop: normalize(80),
+  },
+  plantMascot: {
+    position: 'absolute',
+    alignSelf: 'center',
+    bottom: normalize(13),
+    width: normalize(131, 300),
+    height: undefined,
+    // aspectRatio: width / height,
+    aspectRatio: 1 / 1,
   },
   blueBackground: {
     backgroundColor: '#36549a',
--- components/common/BlueBottomElipse.js
+++ components/common/BlueBottomElipse.js
@@ -2,26 +2,35 @@ import React from 'react';
 import {StyleSheet, Image} from 'react-native';
 import {normalize} from '../../utils/deviceScaling';
 import elipse from '../../assets/images/bottom-elipse-blue.png';
+import propTypes from 'prop-types';
 
 class BlueBottomElipse extends React.Component {
+  static propTypes = {
+    style: propTypes.object,
+  };
   render() {
     return (
       <>
-        <Image source={elipse} style={styles.elipse} />
+        <Image
+          source={elipse}
+          style={[styles.elipse, this.props.style || styles.positon]}
+        />
       </>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  positon: {
+    position: 'absolute',
+    bottom: 0,
+  },
   elipse: {
     width: normalize(375),
     height: undefined,
     // aspectRatio: width / height,
     aspectRatio: 375 / 80,
     alignItems: 'center',
-    position: 'absolute',
-    bottom: 0,
   },
 });
 
