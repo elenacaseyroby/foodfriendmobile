b757dea9610903a92dbc6ad6e12cf636d44a2a70
Casey Roby <elenacaseyroby@gmail.com>
2020-09-24 22:32:36 -0400

    change order of load

--- redux/bulkFetch.js
+++ redux/bulkFetch.js
@@ -7,12 +7,12 @@ import {fetchUserRecipes} from './actions/userRecipesActionCreator';
 import {fetchActivePathRecipes} from './actions/activePathRecipesActionCreator';
 
 export function fetchAllSignedInData(dispatch, userId) {
-  dispatch(fetchUser(userId));
-  dispatch(fetchRecentlyConsumedFoods(userId));
   dispatch(fetchDailyProgress(userId));
+  dispatch(fetchUser(userId));
+  dispatch(fetchNutrients());
+  dispatch(fetchRecentlyConsumedFoods(userId));
   dispatch(fetchUserRecipes(userId));
   dispatch(fetchActivePathRecipes(userId));
-  dispatch(fetchNutrients());
   dispatch(fetchDiets());
   return 'success';
 }
